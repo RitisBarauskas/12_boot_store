d76dd8abb8735d01bf04ae0a939de2c3824558bf
Ritis Barauskas <perm300ru@yandex.ru>
2024-08-13 19:29:08 +0300

    fix

--- goods/views.py
+++ goods/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 
 from goods.models import Good, Category, User
 from goods.forms import CategoryForm, GoodForm
